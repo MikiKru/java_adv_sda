fd6df0458a2f029cbf241a026d63d80fadab3f96
MikiKru <michal_kruczkowski@o2.pl>
2021-04-19 11:59:36 +0200

    java_adv d1.2

--- src/main/java/oop/Admin.java
+++ src/main/java/oop/Admin.java
@@ -1,7 +1,16 @@
 package oop;
 
-import java.time.LocalDate;
+import lombok.*;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+
+//@Data     // automatycznie dodaje gettery, settery i toString w kodzie klasy
+@Getter     // automatycznie dodaje gettery w kodzie klasy
+@Setter     // automatycznie dodaje settery w kodzie klasy
+@ToString   // automatycznie dodaje toString w kodzie klasy
+//@NoArgsConstructor  // automatycznie dodaje domyślny konstruktor w kodzie klasy
+//@AllArgsConstructor // automatycznie dodaje konstruktor z wszystkimi polami klasowymi jako argumenty w kodzie klasy
 public class Admin extends User {
     private String [] priviliges;
     private String name;
@@ -12,7 +21,17 @@ public class Admin extends User {
         this.priviliges = priviliges;
         this.name = name;
         this.lastname = lastname;
+//        super.login = "A";  // nie dziedziczemy składowych prywatnych
         System.out.println("Jestem w kontruktorze Admin");
+    }
+    // ALT + Ins
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "priviliges=" + Arrays.toString(priviliges) +
+                ", name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                "} " + super.toString(); // wywołanie metody z klasy nadrzędnej
     }
 
 }
--- src/main/java/oop/Main.java
+++ src/main/java/oop/Main.java
@@ -4,14 +4,44 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Main {
+    public boolean isActivated(Object o){
+        return ((User) o).isStatus();
+    }
+
     public static void main(String[] args) {
-        User user = new User(
-                "mk",
-                "mk@mk.pl",
-                "mk",
-                LocalDate.of(2000,5,5));
-        System.out.println(user.toString());
-        Admin admin = new Admin("aa","a@a.pl", "aa", LocalDate.of(2002,2,2),
+//        User user = new User("mk", "mk@mk.pl", "mk", LocalDate.of(2000,5,5));
+//        System.out.println(user.toString());
+//        Admin admin = new Admin("aa","a@a.pl", "aa", LocalDate.of(2002,2,2),
+//                new String[]{"R", "W", "A"}, "Admin", "Admin");
+//        admin.setName("Adam");
+//        admin.setStatus(false);
+//        System.out.println(admin);
+        // POLIMORFIZM - wielopostaciowość
+        Object u1 = new User("u", "u@u.pl", "u", LocalDate.of(2005, 5, 1));
+        System.out.println(u1.getClass().getName());
+        Object u2 = new Admin("aa", "a@a.pl", "aa", LocalDate.of(2002, 2, 2),
                 new String[]{"R", "W", "A"}, "Admin", "Admin");
+        System.out.println(u2.getClass().getName());
+        Object u3 = u2;
+        ((Admin) u2).setName("XYZ");
+        System.out.println(u3.getClass().getName());
+        System.out.println(u1.hashCode());
+        System.out.println(u2.hashCode());
+        System.out.println(u3.hashCode());
+        System.out.println(u1);
+        System.out.println(u2);
+        System.out.println(u3);
+        System.out.println(u1 == u2);
+        System.out.println(u2 == u3);
+
+        Main main = new Main();
+        ((User) u1).setStatus(false);
+        System.out.println(main.isActivated(u1));
+        System.out.println(main.isActivated(u2));
+        System.out.println(main.isActivated(u3));
+        System.out.println(main.isActivated(
+                new User("x","x@x.pl","x", LocalDate.of(1999,2,2))
+        ));
+//        System.out.println(main.isActivated("ABC"));
     }
 }
