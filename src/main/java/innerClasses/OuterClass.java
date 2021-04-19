c8dd7d1710fffedbc530bdb41f4a0e1e42b15fc1
MikiKru <michal_kruczkowski@o2.pl>
2021-04-19 15:20:48 +0200

    java_adv d1.10

--- src/main/java/innerClasses/OuterClass.java
+++ src/main/java/innerClasses/OuterClass.java
@@ -7,4 +7,37 @@ import lombok.Data;
 @AllArgsConstructor     // OuterClass(String title){...}
 public class OuterClass {
     private String title;
+    public InnerClass getInnerClassObject(String serialNumber){
+        return new InnerClass(serialNumber);
+    }
+    public InnerClass getInnerClassDefaultObject(){
+        return new InnerClass();
+    }
+    // klasa wewnętrzna
+    @Data
+    public class InnerClass{
+        private String serialNo;
+        // konstruktor klasy wewnętrznej
+        InnerClass(String serialNo) {
+            this.serialNo = serialNo;
+            title = "title from inner class";
+        }
+        InnerClass(){}
+        public void getInnerInfo(){
+            System.out.println("SERIAL: " + serialNo);
+            System.out.println("TITLE: " + title);
+        }
+    }
+    public static void main(String[] args) {
+        OuterClass outerClass = new OuterClass("Example");
+        System.out.println(outerClass);
+        // utworzenie instancji klasy wewnętrznej
+        InnerClass innerClass1 = outerClass.new InnerClass("QWE!@#");
+        // utworzenie instancji na podstawie metody
+        InnerClass innerClass2 = outerClass.getInnerClassObject("QWER$#@!");
+        InnerClass innerClass3 = outerClass.getInnerClassDefaultObject();
+        System.out.println(innerClass2);
+        System.out.println(innerClass3);
+        innerClass2.getInnerInfo();
+    }
 }
