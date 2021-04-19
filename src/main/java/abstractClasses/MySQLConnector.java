fd41188c300c689f90b1f9ae1ce36dad69daab15
MikiKru <michal_kruczkowski@o2.pl>
2021-04-19 14:36:44 +0200

    java_adv d1.8

--- src/main/java/abstractClasses/MySQLConnector.java
+++ src/main/java/abstractClasses/MySQLConnector.java
@@ -1,7 +1,27 @@
 package abstractClasses;
 
+import oop.User;
+
+import java.time.LocalDate;
+
 public class MySQLConnector extends DatabaseConnector {
     // dziedziczę metodę connect
+    private String dbEngine;
+    public boolean connect(String database, String userName, String password){
+        // konstrukcja super
+        super.connect(database,userName,password);
+        if(isConnected()) {
+            dbEngine = "MySQL";
+            System.out.println("Connected to MySQL database");
+            System.out.println("DB name: " + database);
+            System.out.println("DB user: " + userName);
+            System.out.println("DB password: " + password);
+        } else {
+            dbEngine = null;
+        }
+        return isConnected();
+    }
+
     @Override
     public void close() {           // zamyka otwarte połączenie -> isConnected -> false i przypisuje null do parametrów połączenia
         setConnected(false);
@@ -34,8 +54,7 @@ public class MySQLConnector extends DatabaseConnector {
     public static void main(String[] args) {
         DatabaseConnector db = new MySQLConnector();
         db.connect("task_manager","user", "qwe123");
-        db.insert("ABC");
-        db.insert("ZYZ");
+        db.insert(new User("q","q@q.pl", "qqq", LocalDate.of(2000,2,2)));
         for(Object o : db.select()){
             System.out.println(o);
         }
