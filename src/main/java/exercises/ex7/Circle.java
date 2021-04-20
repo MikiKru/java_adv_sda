77765d882f1d7fdacce30cba20133c34d71bf279
MikiKru <michal_kruczkowski@o2.pl>
2021-04-20 09:26:37 +0200

    java_adv d2.0

--- src/main/java/exercises/ex7/Circle.java
+++ src/main/java/exercises/ex7/Circle.java
@@ -1,8 +1,12 @@
 package exercises.ex7;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Circle implements GeometricObject {
     private double radius;
     @Override
--- src/main/java/exercises/ex7/ResizableCircle.java
+++ src/main/java/exercises/ex7/ResizableCircle.java
@@ -1,9 +1,26 @@
 package exercises.ex7;
 
-public class ResizableCircle implements Resize{
-
+public class ResizableCircle extends Circle implements Resize{
+    public ResizableCircle(double radius) {
+        super(radius);
+    }
     @Override
     public void resize(int percent) {
-        
+        super.setRadius(super.getRadius() * ( (double)percent / 100));
+    }
+
+    public static void main(String[] args) {
+        Circle circle1 = new Circle(10);
+        System.out.println("AREA: " + circle1.getArea());
+        System.out.println("PERIMETER: " + circle1.getPerimiter());
+        System.out.println(circle1);
+        Circle circle2 = new ResizableCircle(2);
+        System.out.println("AREA BEFORE: " + circle2.getArea());
+        System.out.println("PERIMETER BEFORE: " + circle2.getPerimiter());
+        System.out.println("BEFORE: " + circle2);
+        ((ResizableCircle) circle2).resize(50);
+        System.out.println("AREA AFTER: " + circle2.getArea());
+        System.out.println("PERIMETER AFTER: " + circle2.getPerimiter());
+        System.out.println("AFTER: " + circle2);
     }
 }
