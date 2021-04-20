29bf1ec20c8ae305e7dde76f4b98152cd2e66403
MikiKru <michal_kruczkowski@o2.pl>
2021-04-20 12:11:46 +0200

    java_adv d2.10

--- src/main/java/sets/SetOperations.java
+++ src/main/java/sets/SetOperations.java
@@ -16,14 +16,29 @@ public class SetOperations {
         resultSet.addAll(set2);
         return resultSet;
     }
-    public Set<Integer> sub(){}
-    public Set<Integer> retain(){}
-    public Set<Integer> symetricSub(){}
+    public Set<Integer> sub(){
+        Set<Integer> resultSet = new HashSet<>(set1);
+        resultSet.removeAll(set2);
+        return resultSet;
+    }
+    public Set<Integer> retain(){
+        Set<Integer> resultSet = new HashSet<>(set1);
+        resultSet.retainAll(set2);
+        return resultSet;
+    }
+    public Set<Integer> symetricSub(){
+        Set<Integer> resultSet = new HashSet<>(add());
+        resultSet.removeAll(retain());
+        return resultSet;
+    }
 
     public static void main(String[] args) {
         SetOperations setOperations = new SetOperations(
                 new HashSet<>(Arrays.asList(1,2,3,4,6,7,8,5,5,5)),
                 new HashSet<>(Arrays.asList(1,4,6,7)));
         System.out.println(setOperations.add());
+        System.out.println(setOperations.sub());
+        System.out.println(setOperations.retain());
+        System.out.println(setOperations.symetricSub());
     }
 }
