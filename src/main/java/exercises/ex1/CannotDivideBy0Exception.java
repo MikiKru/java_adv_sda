53511abf15eab954678e577aab26056dbf8e97e8
MikiKru <michal_kruczkowski@o2.pl>
2021-04-20 15:29:02 +0200

    java_adv d2.21

---
+++ src/main/java/exercises/ex1/CannotDivideBy0Exception.java
@@ -0,0 +1,4 @@
+package exercises.ex1;
+
+public class CannotDivideBy0Exception extends Exception {
+}
--- src/main/java/exercises/ex1/Ex1.java
+++ src/main/java/exercises/ex1/Ex1.java
@@ -1,8 +1,25 @@
 package exercises.ex1;
 
 public class Ex1 {
-//    Zaimplementuj metodę divide , która docelowo ma podzielić dwie liczby
+    //    Zaimplementuj metodę divide , która docelowo ma podzielić dwie liczby
 //    będące atrybutami metody. W przypadku, gdy drugi parametr metody jest równy
 //    0, powinien zostać wyrzucony niedomyślny wyjątek:
 //    CannotDivideBy0Exception .
+    public static Double divide(int num1, int num2) {
+        try {
+            if (num2 == 0) {
+                throw new CannotDivideBy0Exception();
+            }
+            return (double)num1 / num2;
+        } catch (CannotDivideBy0Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(divide(1,2));
+        System.out.println(divide(1,0));
+        System.out.println(divide(1,10));
+    }
+
 }
