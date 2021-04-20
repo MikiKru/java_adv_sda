0ecaa910662c356dd4ddd295c93d1e472a8bb0ec
MikiKru <michal_kruczkowski@o2.pl>
2021-04-20 14:38:34 +0200

    java_adv d2.15

---
+++ src/main/java/exceptions/ExceptionsTrigger.java
@@ -0,0 +1,29 @@
+package exceptions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ExceptionsTrigger {
+    List<Integer> elements = new ArrayList<>(Arrays.asList(1,2,3,4));
+
+    public Integer getElementByIndex(int index){
+        try {                                  // jeśli w try wystąpi wyjątek to przejdź do catch
+                                               // jeżeli nie to wykonaj kod w try i przejdź do kolejnych instrukcji za catch
+            int element = elements.get(index); // identyfikacja miejsca wystąpienia wyjątku
+            return element;
+        } catch (IndexOutOfBoundsException ex){// obsługa wyjątku typu IndexOutOfBoundException
+            ex.printStackTrace();
+            return null;
+        } catch (Exception ex){                // obsługa wyjątku typu Exception - każdego wyjątku
+            ex.printStackTrace();
+            return null;
+        }
+    }
+    public static void main(String[] args) {
+        ExceptionsTrigger et = new ExceptionsTrigger();
+        System.out.println(et.getElementByIndex(20));
+        System.out.println(et.getElementByIndex(2));
+        System.out.println(et.getElementByIndex(1));
+    }
+}
