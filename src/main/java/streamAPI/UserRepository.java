a9ffa09604a3534b4a73c80d5eb3ae52050f0944
MikiKru <michal_kruczkowski@o2.pl>
2021-04-21 12:47:41 +0200

    java_adv d3.14

--- src/main/java/streamAPI/UserRepository.java
+++ src/main/java/streamAPI/UserRepository.java
@@ -3,9 +3,7 @@ package streamAPI;
 import oop.User;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public interface UserRepository {
     List<User> users = new ArrayList<>(Arrays.asList(
@@ -14,9 +12,26 @@ public interface UserRepository {
             new User("c", "c@c.pl", "cc", LocalDate.of(1998,4,16)),
             new User("d", "d@d.pl", "add", LocalDate.of(2000,2,13)),
             new User("e", "e@e.pl", "a32", LocalDate.of(2003,6,1)),
-            new User("f", "f@f.pl", "adsa", LocalDate.of(2000,,81)),
+            new User("f", "f@f.pl", "adsa", LocalDate.of(2000,8,1)),
             new User("g", "g@g.pl", "adsa", LocalDate.of(2002,12,1)),
             new User("h", "h@h.pl", "ad", LocalDate.of(2000,11,13)),
             new User("i", "i@i.pl", "a", LocalDate.of(2001,2,10))
     ));
+    List<User> getAllUsers();
+    // pobiera listę użytkowników posortowanych po filedName, isAsc determinuje typ sortowania
+    List<User> getAllUsersOrderBy(String filedName, boolean isAsc);
+    // pobierz użytkownika po adresie email
+    Optional<User> getUserByEmail(String email);
+    // zwróć tabele użytkowników pogrupowanych po roku urodzenia
+    // 2000 : 3
+    // 1999 : 1
+    // ....
+    Map<Integer, User> getUsersGroupingByYears();
+    // Zwróć dane o użytkownikach w typie string jak poniżej:
+    // login | email | password | bithDate |
+    // ...   | ...   | ....     | ...      |
+    String getUsersInfo();
+    // zwróć listę użytkowników z hasłem zaszyfrowanym wybranym algorytmem
+    List<User> getUsersWithEncodedPassword(String algorithmName);
+
 }
