1e05bca7b6f316596562978cecf3c885d9d49bf2
MikiKru <michal_kruczkowski@o2.pl>
2021-04-21 12:32:52 +0200

    java_adv d3.13

--- src/main/java/streamAPI/StreamAPINumbers.java
+++ src/main/java/streamAPI/StreamAPINumbers.java
@@ -1,6 +1,8 @@
 package streamAPI;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class StreamAPINumbers {
     public static void main(String[] args) {
@@ -13,5 +15,15 @@ public class StreamAPINumbers {
         Arrays.stream(numbers).max().ifPresentOrElse(
                 value -> System.out.println("MAX: " + value),
                 () -> System.out.println("NO MAX VALUE - EMPTY LIST"));
+        // zwróć sumę wszystkich elementów z tablicy
+        System.out.println("SUM: " + Arrays.stream(numbers).sum());
+        // zwróć średnią z elementów tablicy
+        Arrays.stream(numbers).average().ifPresentOrElse(
+                value -> System.out.println("AVG: " + value),
+                () -> System.out.println("NO AVG - EMPTY LIST"));
+        // grupowanie -> jaka jest krotność występowania poszczególnych liczb
+//        System.out.println(Arrays.stream(numbers)
+//                .collect(Collectors.groupingBy()));
+
     }
 }
