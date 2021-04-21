50f96c73cca505c5f753481946391d4fad4efae5
MikiKru <michal_kruczkowski@o2.pl>
2021-04-21 15:34:40 +0200

    java_adv d3.25

--- src/main/java/enums/Category.java
+++ src/main/java/enums/Category.java
@@ -1,5 +1,17 @@
 package enums;
 
+import lombok.Getter;
+
+@Getter
 public enum Category {
-    PROGRAMMING, DEV_OPS, SCRUM, TESTING
+    PROGRAMMING("programowanie"),
+    DEV_OPS("administracja"),
+    SCRUM("scrum"),
+    TESTING("testowanie");
+
+    private final String name;
+
+    Category(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/enums/Task.java
+++ src/main/java/enums/Task.java
@@ -1,5 +1,10 @@
 package enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
 public class Task {
     private String title;
     private Category category;
