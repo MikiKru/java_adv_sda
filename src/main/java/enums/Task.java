cea302be653c3cfca97715a9dbf86748f61dbbe9
MikiKru <michal_kruczkowski@o2.pl>
2021-04-21 15:48:40 +0200

    java_adv d3.26

--- src/main/java/enums/Task.java
+++ src/main/java/enums/Task.java
@@ -3,10 +3,19 @@ package enums;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@AllArgsConstructor
+import java.util.Arrays;
+
 @Data
 public class Task {
     private String title;
     private Category category;
     private Status status;
+
+    public Task(String title, String categoryName, Status status) {
+        this.title = title;
+        this.category = Arrays.stream(Category.values())
+                .filter(c -> c.getName().equals(categoryName))
+                .findFirst().get();
+        this.status = status;
+    }
 }
