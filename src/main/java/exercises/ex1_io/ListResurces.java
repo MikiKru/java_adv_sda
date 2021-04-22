43d5dd1e0aa1704bcdf80ef5a42bfb91e6dff1a1
MikiKru <michal_kruczkowski@o2.pl>
2021-04-22 12:15:31 +0200

    java_adv d4.14

--- src/main/java/exercises/ex1_io/ListResurces.java
+++ src/main/java/exercises/ex1_io/ListResurces.java
@@ -1,15 +1,23 @@
 package exercises.ex1_io;
 
 import java.io.File;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.stream.Collectors;
+
 
 //Napisz program, który wyświetli wszystkie pliki/katalogi zawarte w danym katalogu.
 //Wraz z informacją o plikach i katalogach zwróć informację o ich rozmiarze i dacie utworzeni
 public class ListResurces {
     public static void main(String[] args) {
-        File file = new File("C:\\Users\\PROXIMO\\Desktop\\java_adv_sda\\src\\main\\java\\exercises");
-        Arrays.stream(file.list()).forEach(System.out::println);
+        final String PATH = Paths.get("").toAbsolutePath().toString() + "\\src\\main\\java\\exercises";
+        File file = new File(PATH);
+        Arrays.stream(file.list())
+                .forEach(f -> System.out.printf("| %20s | %10skb |\n",
+                        f,
+                        new File( PATH + "\\" + f).isDirectory() ?
+                                "0" :
+                                new File( PATH + "\\" + f).length()));
 
     }
 }
