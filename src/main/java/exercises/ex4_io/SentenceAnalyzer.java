c4ce0978f83d4c3f2af5184396a53d73bdf4e7e7
MikiKru <michal_kruczkowski@o2.pl>
2021-04-22 12:50:48 +0200

    java_adv d4.17

--- src/main/java/exercises/ex4_io/SentenceAnalyzer.java
+++ src/main/java/exercises/ex4_io/SentenceAnalyzer.java
@@ -1,5 +1,38 @@
 package exercises.ex4_io;
 
-public class SentenceAnalyzer {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
 
+public class SentenceAnalyzer {
+    private static final String PATH = Paths.get("").toAbsolutePath().toString() +
+            "\\src\\main\\java\\exercises\\ex4_io\\definition.txt";
+    public static String getLongestWordInFile() {
+        try {
+            Scanner sc = new Scanner(new File(PATH));
+            String longestWord = "";
+            String[] dividedSentence;
+            String wordToCompare;
+            while (sc.hasNextLine()) {
+                dividedSentence = sc.nextLine().replaceAll("[\\.,()]", "").split(" ");
+                wordToCompare = Arrays.stream(dividedSentence).max(Comparator.comparing(String::length)).get();
+                if (wordToCompare.length() > longestWord.length()) {
+                    longestWord = wordToCompare;
+                }
+            }
+            sc.close();
+            return longestWord;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.err.println("Nie znaleziono pliku!");
+        }
+        return null;
+    }
+    public static void main(String[] args) {
+        System.out.println(getLongestWordInFile());
+        System.out.println(getLongestWordInFile().length());
+    }
 }
