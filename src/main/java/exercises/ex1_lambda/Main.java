474c73af58a1153eb95def096f15a6cb775012e4
MikiKru <michal_kruczkowski@o2.pl>
2021-04-22 10:45:21 +0200

    java_adv d4.7

--- src/main/java/exercises/ex1_lambda/Main.java
+++ src/main/java/exercises/ex1_lambda/Main.java
@@ -5,11 +5,18 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Video v1 = new Video("Ex", "ex.pl/ex", VideoType.EPISODE);
-        System.out.println(v1);
-        Episode e1 = new Episode("E1",1, new ArrayList<>(Arrays.asList(v1)));
-        System.out.println(e1);
-        Season s1 = new Season("S1", 1, new ArrayList<>(Arrays.asList(e1)));
-        System.out.println(s1);
+//        Video v1 = new Video("Ex", "ex.pl/ex", VideoType.EPISODE);
+////        System.out.println(v1);
+////        Episode e1 = new Episode("E1",1, new ArrayList<>(Arrays.asList(v1)));
+////        System.out.println(e1);
+////        Season s1 = new Season("S1", 1, new ArrayList<>(Arrays.asList(e1)));
+////        System.out.println(s1);
+        SerialRepository sr = new SerialRepositoryImpl();
+        System.out.println("EPISODES");
+        sr.getAllEpisodes(SerialData.seasons).forEach(System.out::println);
+        System.out.println("VIDEOS");
+        sr.getAllVideos(SerialData.seasons).forEach(System.out::println);
+        System.out.println("VIDEO NAMES");
+        sr.getAllVideoNames(SerialData.seasons).forEach(System.out::println);
     }
 }
--- src/main/java/exercises/ex1_lambda/SerialRepositoryImpl.java
+++ src/main/java/exercises/ex1_lambda/SerialRepositoryImpl.java
@@ -6,17 +6,22 @@ import java.util.stream.Collectors;
 public class SerialRepositoryImpl implements SerialRepository {
     @Override
     public List<Episode> getAllEpisodes(List<Season> seasons) {
-        return SerialData.seasons.stream()                  // Stream<Season>
+        return seasons.stream()                             // Stream<Season>
                 .flatMap(s -> s.getEpisodes().stream())     // Stream<Season> -> Streams<Episode>
                 .collect(Collectors.toList());              // List<Episodes>
     }
     @Override
     public List<Video> getAllVideos(List<Season> seasons) {
-        return null;
+        return seasons.stream()                                 //stream season
+                .flatMap(s -> s.getEpisodes().stream())        //stream <Season> -> Streams<Episode>
+                .flatMap(e -> e.getVideos().stream())          //stream <Video>
+                .collect(Collectors.toList());
     }
     @Override
     public List<String> getAllVideoNames(List<Season> seasons) {
-        return null;
+        return getAllVideos(seasons).stream()                   // Stream<Video>
+                .map(video -> video.getTitle())                 // Stream<String>
+                .collect(Collectors.toList());
     }
     @Override
     public List<Video> getAllVideosWithEvenSeasonsAndEvenEpisodes(List<Season> seasons) {
