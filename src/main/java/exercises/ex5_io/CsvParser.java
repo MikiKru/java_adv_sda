d431af7e6b83a94495c0fe148e2088ea69b0cb70
MikiKru <michal_kruczkowski@o2.pl>
2021-04-22 13:51:37 +0200

    java_adv d4.20

---
+++ src/main/java/exercises/ex5_io/CsvParser.java
@@ -0,0 +1,18 @@
+package exercises.ex5_io;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CsvParser {
+    private List<User> users = new ArrayList<>();
+    private final String PATH = "???";
+
+    // prasowanie CSV to User
+    private List<User> getUsersFromCsvFile(){
+        return null;
+    }
+    // parsowanie User to CSV
+    private void addUsertoCsvFile(User user){
+
+    }
+}
