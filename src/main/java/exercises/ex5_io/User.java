54d7c0c7f20e991ad89a193e47edbdec36257e12
MikiKru <michal_kruczkowski@o2.pl>
2021-04-22 13:46:25 +0200

    java_adv d4.19

---
+++ src/main/java/exercises/ex5_io/User.java
@@ -0,0 +1,12 @@
+package exercises.ex5_io;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class User {
+    private String name;
+    private String lastName;
+    private int age;
+}
