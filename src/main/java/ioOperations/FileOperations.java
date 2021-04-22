9fa9f95a536d77b82bbc2044e141ddefdced53b0
MikiKru <michal_kruczkowski@o2.pl>
2021-04-22 11:32:40 +0200

    java_adv d4.10

--- src/main/java/ioOperations/FileOperations.java
+++ src/main/java/ioOperations/FileOperations.java
@@ -12,7 +12,10 @@ public class FileOperations {
     public void addNote(String note){
         try {
             PrintWriter pw = new PrintWriter("mynotes.txt");
-            pw.println(note);
+            notes.add(note);                                // dodaję notatkę do listy
+            for(String s : notes) {
+                pw.println(s);                               // aktualizuję plik na bazie listy
+            }
             pw.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -25,6 +28,7 @@ public class FileOperations {
     }
     public static void main(String[] args) {
         FileOperations fo = new FileOperations();
-
+        fo.addNote("Nauka STREAM API");
+        fo.addNote("Nauka FileIO");
     }
 }
