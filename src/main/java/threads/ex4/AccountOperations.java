9da5e05f4e6b95dab8138f9753875bc80458ee42
MikiKru <michal_kruczkowski@o2.pl>
2021-04-26 15:40:20 +0200

    java_adv d5.22

---
+++ src/main/java/threads/ex4/AccountOperations.java
@@ -0,0 +1,55 @@
+package threads.ex4;
+
+import lombok.AllArgsConstructor;
+
+import java.util.Random;
+
+@AllArgsConstructor
+public class AccountOperations {
+    private Accout accout;
+
+    public double getAmount(){
+        return accout.getAmount();
+    }
+    public void cacheIn(double amount){
+        accout.setAmount(accout.getAmount() + amount);
+        System.out.println("Zasilono konto! Twoje Saldo: " + getAmount());
+    }
+    public void cacheOut(double amount){
+        if(amount <= accout.getAmount()) {
+            accout.setAmount(accout.getAmount() - amount);
+            System.out.println("Wykonano przelew wychodzący! Twoje saldo: "+getAmount());
+        } else {
+            System.out.println("Nie możesz wypłacić kwoty: " + amount + " Twoje saldo: " + getAmount());
+        }
+    }
+    public static void main(String[] args) {
+        Accout accout = new Accout(1000);
+        AccountOperations ac = new AccountOperations(accout);
+        Thread cacheIn = new Thread(() -> {
+            Random random = new Random();
+            for(int i = 0; i < 10; i++){
+                ac.cacheIn(random.nextInt(1000));
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        Thread cacheOut = new Thread(() -> {
+            Random random = new Random();
+            for(int i = 0; i < 12; i++){
+                ac.cacheOut(random.nextInt(1000));
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        cacheIn.start();
+        cacheOut.start();
+
+    }
+}
---
+++ src/main/java/threads/ex4/Accout.java
@@ -0,0 +1,10 @@
+package threads.ex4;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Accout {
+    private double amount;
+}
