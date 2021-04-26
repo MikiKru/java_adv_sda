b489fa0a97cadc8da9582bc8a51b57ff43d3a177
MikiKru <michal_kruczkowski@o2.pl>
2021-04-26 15:57:29 +0200

    java_adv d5.23

--- src/main/java/threads/ex4/AccountOperations.java
+++ src/main/java/threads/ex4/AccountOperations.java
@@ -15,7 +15,9 @@ public class AccountOperations {
         accout.setAmount(accout.getAmount() + amount);
         System.out.println("Zasilono konto! Twoje Saldo: " + getAmount());
     }
-    public void cacheOut(double amount){
+    // synchronized - pozwala na wykonywanie metody w całości tylko dla jednego wątku
+    // powoduje tworzenie sekcji krytycznej
+    public synchronized void cacheOut(double amount){
         if(amount <= accout.getAmount()) {
             accout.setAmount(accout.getAmount() - amount);
             System.out.println("Wykonano przelew wychodzący! Twoje saldo: "+getAmount());
@@ -48,8 +50,19 @@ public class AccountOperations {
                 }
             }
         });
+        Thread cacheOut2 = new Thread(() -> {
+            Random random = new Random();
+            for(int i = 0; i < 12; i++){
+                ac.cacheOut(random.nextInt(1000));
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
         cacheIn.start();
         cacheOut.start();
-
+        cacheOut2.start();
     }
 }
